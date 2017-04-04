275840e36983c6f4bf13ff42d3c621457d5d5cd8
Yago Rodrigues <yagohsro@gmail.com>
2017-04-04 14:41:03 -0300

    Alteração JOptionPane

--- src/main/java/com/tdsis/casas/App.java
+++ src/main/java/com/tdsis/casas/App.java
@@ -1,5 +1,7 @@
 package com.tdsis.casas;
 
+import javax.swing.JOptionPane;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JOptionPane.showMessageDialog(null, "Hello World!" );
     }
 }
