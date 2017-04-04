913d34058d1f7e8eee55ca8a3dc2b0eab0e886ea
Yago Rodrigues <yagohsro@gmail.com>
2017-04-04 15:31:03 -0300

    Multiplicação

--- src/main/java/com/tdsis/casas/App.java
+++ src/main/java/com/tdsis/casas/App.java
@@ -6,10 +6,19 @@ import javax.swing.JOptionPane;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        JOptionPane.showMessageDialog(null, "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		int num1 = 0, num2 = 0, resultado = 0;
+		JOptionPane.showMessageDialog(null, "Hello World!");
+		try {
+			num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para multiplicação"));
+			num2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número inteiro para multiplicação"));
+			resultado = num1 * num2;
+			JOptionPane.showMessageDialog(null, "O resultado é " + resultado);
+		} catch (Exception e) {
+			// TODO: handle exception
+			JOptionPane.showMessageDialog(null, "Número inválido!!");
+		}
+
+	}
 }
